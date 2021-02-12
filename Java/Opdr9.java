39fdab9bb5410676f90dc20c0d109a5a250182cc
Willemc96 <76949068+Willemc96@users.noreply.github.com>
2021-02-12 18:05:11 +0100

    Add files via upload

---
+++ Java/Opdr9.java
@@ -0,0 +1,38 @@
+public class Opdr9 {
+	
+	public static void main(String[] args) {
+		Vliegtuig mijnVliegtuig = new Vliegtuig(); 
+		mijnVliegtuig.landen();
+		
+		Spreeuw mijnSpreeuw = new Spreeuw();
+		mijnSpreeuw.opstijgen();
+		  }
+	
+}
+	
+	interface Vliegend {
+		void opstijgen(); 
+		void landen(); 
+	}
+	
+	class Vliegtuig implements Vliegend {
+		public void opstijgen() {
+	    System.out.println("Het vliegtuig is opgestegen");
+	  }
+		public void landen() {
+	    System.out.println("Het vliegtuig is geland");
+	  }
+	}
+	
+	class Spreeuw implements Vliegend{
+		public void opstijgen() {
+		    System.out.println("De spreeuw is weggevlogen");
+		  }
+		public void landen() {
+		    System.out.println("De spreeuw is weer terug");
+		  }
+	}
+	
+	class Leeuw{
+		
+	}
